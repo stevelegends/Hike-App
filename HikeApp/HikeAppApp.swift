2363964188b4651f568f3e7334ee6ff52b2dffb5
hiep <hiep@192.168.1.6>
2024-06-21 14:50:35 +0700

    init swiftUI

---
+++ HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by hiep on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
